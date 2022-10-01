0fad7c14b749111869c83db0476a44b860815191
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-10-01 08:34:49 -0700

    added new projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Github from '../img/Github copy.svg';
 import Linkedin from '../img/linkedin.svg';
+import Medium from '../img/medium.svg';
+import Twitter from '../img/twitter.svg';
 
 export const Footer = () => {
     return (
         <div className='footer'>
             <h2>Find me on</h2>
             <div className='footer-icons'>
-                <a href='#'><img src={Github} width="30px" height="30px" alt='github icon'></img> </a>
+                <a href='https://github.com/Priyanka-koraganji'><img src={Github} width="30px" height="30px" alt='github icon'></img> </a>
                 <a href='#'><img src={Linkedin} width="40px" height="35px" alt='linkedin icon'></img> </a>
+                <a href='https://medium.com/@priya.koraganji04'><img src={Medium} width="40px" height="40px" alt='linkedin icon'></img> </a>
+                <a href='https://twitter.com/priya_koraganji'><img src={Twitter} width="30px" height="30px" alt='linkedin icon'></img> </a>
             </div>
         </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,18 @@ import moviesApp from '../img/movies-app.png';
 import moviesUser from '../img/userInfo-moviesApp.png';
 import Pokedox from '../img/Pokedox.png';
 import Todo from '../img/Todo.png';
+import BackendAPI from '../img/backend-api.png';
+import AngularMovies from '../img/Angular-movies.png';
+import angularDialog from '../img/angular-dialog.png';
+import MoviesList from '../img/movies-list.png';
+import OfflineChat from '../img/offline-chatApp.png';
+import ChatApp from '../img/chat-app.png';
 
 export const Projects = () => {
     return (
         <div className='projects-page' id="projects">
             <h1>Projects</h1>
+
             <div className='meet-app project'>
                 <div className='project-images'>
                     <img src={meetupLogin} width="100%" alt="meet-up login page"></img>
@@ -32,6 +39,7 @@ export const Projects = () => {
                             <li>Puppeteer</li>
                             <li>Recharts</li>
                             <li>Axios</li>
+                            <li>AWS</li>
                         </ul>
                     </div>
 
@@ -41,10 +49,47 @@ export const Projects = () => {
                     </div>
                 </div>
             </div>
+
+            <div className='myFlix-api project'>
+                <div className='project-images'>
+                    <img className='api-img' src={BackendAPI} width="100%" alt="mongoDB collections"></img>
+                    <img className='api-img' src={MoviesList} width="100%" alt="mongoDB collections"></img>
+                </div>
+                <div>
+                    <h3>MyFlix API</h3>
+                    <div className='project-brief'>
+                        <p>Its a Server side application.</p>
+                        <p>This web application will provide users with access to information about different
+                            movies, directors, and genres.</p>
+                        <p>It was built with JavaScript running on Node.js with Express framework.</p>
+                        <p>Used MongoDB non-relational database to store and retrive data. And have used Mongoose for business logic.</p>
+                        <p>All the routes were checked using Postman.</p>
+                        <p>User authentication was built with Passport. And it is deployed using Heroku.</p>
+                        <p>Tools and Technologies Used:</p>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>Node JS</li>
+                            <li>Express</li>
+                            <li>Mongoose</li>
+                            <li>MongoDB</li>
+                            <li>Postman</li>
+                            <li>Passport</li>
+                            <li>Heroku</li>
+                        </ul>
+                    </div>
+
+                    <div className='project-btn'>
+                        <button><a href='https://github.com/Priyanka-koraganji/movies-app.git'>Github Repo</a></button>
+                        <button><a href='https://yourmoviescollection.herokuapp.com/'>Visit Site</a></button>
+                    </div>
+                </div>
+
+            </div>
+
             <div className='movies-app project'>
                 <div className='project-images'>
-                    <img src={moviesApp} width="100%" alt="movies app"></img>
-                    <img src={moviesUser} width="100%" alt="movies app"></img>
+                    <img className='img' src={moviesApp} width="100%" alt="movies app"></img>
+                    <img className='img' src={moviesUser} width="100%" alt="movies app"></img>
                 </div>
                 <div>
                     <h3>Movies App</h3>
@@ -75,9 +120,73 @@ export const Projects = () => {
                     </div>
                 </div>
             </div>
+
+            <div className='movie-app-angular project'>
+                <div className='project-images'>
+                    <img src={AngularMovies} width="100%" alt="movies app"></img>
+                    <img src={angularDialog} width="100%" alt="movies app"></img>
+                </div>
+                <div>
+                    <h3>Movies App Using Angular</h3>
+                    <div className='project-brief'>
+                        <p>Its a client side web application
+                            based on server-side REST API and database built with Angular and Material Desgin.</p>
+                        <p>This web application will provide users with access to information about different
+                            movies, directors, and genres.</p>
+                        <p>Users will be able to sign up, register, update their
+                            personal information, view data about the movies as well as create a list of their favorite movies.</p>
+                        <p>Checkout the Backend Code on  <a href='https://github.com/Priyanka-koraganji/movies-app'>Github</a> </p>
+                        <p>Tools and Technologies Used:</p>
+                        <ul>
+                            <li>Angular</li>
+                            <li>Typescript</li>
+                            <li>Angular CLI</li>
+                            <li>Material Design</li>
+                            <li>Angular-cli-ghpages</li>
+                            <li>typedoc</li>
+                        </ul>
+                    </div>
+
+                    <div className='project-btn'>
+                        <button><a href='https://github.com/Priyanka-koraganji/MyFlix-Angular.git'>Github Repo</a></button>
+                        <button><a href='https://priyanka-koraganji.github.io/MyFlix-Angular/'>Visit Site</a></button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='chat-app project'>
+                <div className='chat-images'>
+                    <img src={ChatApp} width="100%" alt="movies app"></img>
+                    <img src={OfflineChat} width="100%" margin-left="20px" alt="movies app"></img>
+                </div>
+                <div>
+                    <h3>Chat App</h3>
+                    <div className='project-brief'>
+                        <p>It was developed with React Native, supported by Expo</p>
+                        <p>The app provides users with a chat interface and options to share images and their location.</p>
+                        <p>It uses the services of Google Firestore for storing/retrieving data and for authentication.</p>
+                        <p>Chat mobile application also provides offline functionality.</p>
+                        <p>Tools and Technologies Used:</p>
+                        <ul>
+                            <li>React</li>
+                            <li>Gifted chat</li>
+                            <li>Expo</li>
+                            <li>Google Firebase</li>
+                            <li>IOS and Android simulators</li>
+                        </ul>
+                    </div>
+
+                    <div className='project-btn'>
+                        <button><a href='https://github.com/Priyanka-koraganji/Chat-App.git'>Github Repo</a></button>
+                    </div>
+                </div>
+
+            </div>
+
             <div className='pokedox-app project'>
                 <div className='project-images'>
-                    <img src={Pokedox} width="100%" alt=""></img>
+                    <img className='poke-img' src={Pokedox} width="100%" alt=""></img>
                 </div>
                 <div>
                     <h3>Pokedox App</h3>
